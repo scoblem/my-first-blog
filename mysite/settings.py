b46470153014b50136324fa99acd3fa7033425d6
Mark Scoble <scoble.mr@gmail.com>
2015-09-07 22:03:33 +1000

    Added user authentication.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^@dpxx!=abx1eqeowev8@s4$wiu=a)2detdqhizmq5d+^=tw%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.0.12', '127.0.0.1', 'necrolisk.pythonanywhere.com']
 LOGIN_REDIRECT_URL = '/'
